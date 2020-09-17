f2e760d8adca30ba76f97044b8d0f43dfc206d62
Yuval Beiser <yuval2604@gmail.com>
2020-09-17 11:34:44 +0300

    describe pull&push methids

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 MONGO_URI = "mongodb://user:Aa123456@ds055980.mlab.com:55980/flaskinterview"
-client = pymongo.MongoClient(MONGO_URI)
+client = pymongo.MongoClient(MONGO_URI, retryWrites=False)
 db = client.get_default_database('flaskinterview')
+
 user_records = db.user_records
+#
 
 
 @app.route('/')
@@ -19,14 +21,19 @@ def hello():
     return "Hello World!"
 
 
+# To pull data from collection use
+# db.user_records.find_one({'_id': ObjectId('5f631c6f9e6f2c95e3f688d5')} )
+
 def getRecord(user_id):
     record = user_records.find_one({"user_id": user_id})
     return record
 
+# To push data to the collection
+
 
 def pushRecord(user_id, name, Age, college):
     record = {
-        "user_id": user_id,
+        "id": user_id,
         "name": name,
         "Age": Age,
         "college": college
