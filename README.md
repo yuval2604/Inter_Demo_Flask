ee1bcf87a4c57bbfb37cbf62c3591bf657ed5e15
Yuval Beiser <yuval2604@gmail.com>
2020-09-17 11:10:44 +0300

    connect MongoDB

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # Inter_Demo_Flask
-My Flask website 
+
+My Flask website
 
 The task is to write a simple rest API backend system that responsible of
 handling messages between users.
 a message contains :
+
 1. sender (owner)
 2. receiver
 3. message
 4. subject
 5. creation date
-The rest API should contains :
+   The rest API should contains :
+
 - Write message
 - Get all messages for a specific user
 - Get all unread messages for a specific user
 - Read message (return one message)
 - Delete message (as owner or as receiver)
+
+Deploy a new MongoDB(mlab) project
--- app.py
+++ app.py
@@ -1,10 +1,38 @@
 from flask import Flask
+import pymongo
+from pymongo import MongoClient
 app = Flask(__name__)
+
+MONGO_URI = "mongodb://user:Aa123456@ds055980.mlab.com:55980/flaskinterview"
+client = pymongo.MongoClient(MONGO_URI)
+db = client.get_default_database('flaskinterview')
+user_records = db.user_records
 
 
 @app.route('/')
 def hello():
+    record = getRecord("5f627ac9e7179a472eae094e")
+    print(record)
+    pushRecord(1, "Amit", "16", "OPEN")
+    record = getRecord(1)
+    print(record)
     return "Hello World!"
+
+
+def getRecord(user_id):
+    record = user_records.find_one({"user_id": user_id})
+    return record
+
+
+def pushRecord(user_id, name, Age, college):
+    record = {
+        "user_id": user_id,
+        "name": name,
+        "Age": Age,
+        "college": college
+    }
+    user_records.insert_one(record)
+
 
 if __name__ == '__main__':
     app.run()
